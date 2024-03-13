afb56516717803f397cb5a7a9b5a6bc081f1d622
Slimer <163108719+local-slime@users.noreply.github.com>
2024-03-13 12:59:21 +0100

    Add files via upload

--- Java/JavaProjects/FirstProject/src/Main.java
+++ Java/JavaProjects/FirstProject/src/Main.java
@@ -4,5 +4,24 @@ public class Main {
         System.out.print("Does this work ?");
         System.out.println("\nJava is funky"); //commenty comment
         System.out.println ("Wawa\nWewe");
+
+        //time for variables
+
+        boolean mytest = true;
+        var e = "Hi?"; //figure out why var exists later
+        String E = e;
+        int x = 8;
+        double y = 2.25;
+        float z = 1.48f; //why do float need a special thing ;( oh well
+        if (mytest) {
+            System.out.println("Did I do this correctly ?");
+            System.out.println("var e = "+e);
+            System.out.println("var E = "+E);
+            System.out.println(x);//what about numbers
+            System.out.println(y);
+            System.out.println(y-x);
+            double zMinY = (z-y);
+            System.out.println("var zMinY = "+zMinY);
+        }
     }
 }
