7668d75042a8f45b301d9ad14f9b8190fbfb64df
Slimer <163108719+local-slime@users.noreply.github.com>
2024-03-13 12:40:34 +0100

    Added files with upload
    yeah

---
+++ Java/JavaProjects/FirstProject/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello\n");
+        System.out.print("Does this work ?");
+        System.out.print("\nJava is funky");
+    }
+}
