753f6ec00e6f9fce65f8c2f7d7be98ef8b58c7a4
Slimer <163108719+local-slime@users.noreply.github.com>
2024-03-13 12:48:57 +0100

    Replace the files hopefully

--- Java/JavaProjects/FirstProject/src/Main.java
+++ Java/JavaProjects/FirstProject/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.print("Hello\n");
         System.out.print("Does this work ?");
-        System.out.print("\nJava is funky");
+        System.out.println("\nJava is funky"); //commenty comment
+        System.out.println ("Wawa\nWewe");
     }
 }
